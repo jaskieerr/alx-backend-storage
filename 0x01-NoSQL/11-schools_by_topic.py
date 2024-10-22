ee92d269a534cec9896283d2c71b32f3d75c3842
jaskieerr <nizar.zairat@gmail.com>
2024-10-22 05:38:40 -0700

    ss

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+find find
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    find find find
+    '''
+    return mongo_collection.find({"topics": topic})
