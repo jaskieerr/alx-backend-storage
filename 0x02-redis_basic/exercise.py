11edf15dc183a8cb9013493268f9e3cf0d06a948
jaskieerr <nizar.zairat@gmail.com>
2024-10-23 10:32:32 +0000

    testt

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+task0 yes task0
+'''
+import redis
+import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+class Cache:
+    '''
+    task0
+    '''
+    def __init__(self):
+        '''constructor'''
+        self._redis = redis.redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''doc odc doc'''
+        keyy = str(uuid.uuid4)
+        self._redis.set(keyy, data)
+        return keyy
