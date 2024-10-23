67fabeecd9245b4c39d771a7dec3c1e0ff287331
jaskieerr <nizar.zairat@gmail.com>
2024-10-23 10:51:55 +0000

    yabich

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,23 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    '''
+    counts Cache methods calls
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        '''
+        counts Cache methods calls
+        '''
+
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
 class Cache:
     '''
     task0
